0044e3aa709cb6ade4cbc48d2f8b59b1ed5a3114
em-edu <81432745+em-edu@users.noreply.github.com>
2022-05-10 19:35:05 -0400

    complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,10 +1,62 @@
- //import { gsap } from "gsap";
+import { gsap } from "gsap";
 
 
-//const mainTL = gsap.timeline();
+const mainTL = gsap.timeline();
+//Percent
+let PERC = {num:0};
+let PERC_num = document.querySelector("#percentage_txt tspan");
 
 
 
-// mainTL.add()
+function setStage(){
+    let tl = gsap.timeline();
 
-// ;
+    tl.set("#titletop",{x:"-=175", alpha:0})
+    .set("#titlebottom",{x:"+=175", alpha:0})
+
+    ;
+
+    return tl;
+}
+//CLIPPATH
+function showTitle(){
+    let tl = gsap.timeline({onComplete:showBolts});
+
+    tl.to("#titletop",{duration:0.5, x:"+=175", alpha:1, ease:"back.out"},0)
+    .to("#titlebottom",{duration:0.5, x:"-=175", alpha:1, ease:"back.out"},0)
+
+    ;
+
+    return tl;
+}
+
+function showBolts(){
+
+    gsap.to(".bolt", {duration:3, y:"-=255", repeat:-1, stagger:0.6});
+
+}
+
+function countUpNumbers(){
+    let tl = gsap.timeline();
+
+    tl.to(PERC,{duration:4, num:"+=39", roundProps:"num", onUpdate:percentHandler, ease:"expo.out"})
+
+    ;
+
+    return tl;
+}
+
+function percentHandler(){
+
+        //console.log("percentHandler ran");
+        PERC_num.textContent = PERC.num;
+
+}
+
+
+
+mainTL.add(setStage())
+.add(showTitle())
+.add(countUpNumbers())
+
+;
