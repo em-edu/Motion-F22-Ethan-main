5a6d5e63591b18ae085ea6fc22b73c8ad1130899
em-edu <81432745+em-edu@users.noreply.github.com>
2022-05-26 19:50:07 -0400

    complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,12 +8,41 @@ import { gsap } from "gsap";
 */
 
 const mainTL = gsap.timeline({delay:1});
+const boxColor = gsap.utils.wrap(["red", "blue", "green"]);
+const boxX = gsap.utils.wrap([200, 400, 600]);
 
 //gsap.to(".blue-box",{ duration:3, rotation:360, transformOrigin:"center", repeat:-1, ease:"none"});
 
-mainTL.to(".red-box",{duration:1, x:500, ease:"power1.out"}, "box")
-.to(".blue-box",{duration:1, x:500, ease:"power3.out"}, "box")
-.to(".green-box",{duration:1, x:500, ease:"back.inOut"}, "box")
+mainTL.to(".box",{duration:1, backgroundColor:boxColor, ease:"power1.out"}, "box")//x:boxX,
 
+//without keyframes
+// .to(".red-box",{duration:0.5, x:200, y:0, ease:"power1.out"})
+// .to(".red-box",{duration:0.5, x:200, y:200, ease:"power1.out"})
+// .to(".red-box",{duration:0.5, x:0, y:200, ease:"power1.out"})
+// .to(".red-box",{duration:0.5, x:0, y:0, ease:"power1.out"})
+//with keyframes
+//keyframes ARRAY
+// .to(".red-box",{keyframes:{
+//     x:[0,200,200,0,0],
+//     y:[0,0,200,200,0],
+//     easeEach:"back.out"
+// },
+//duration:2})
+//keyframes OBJECT
+// .to(".red-box",{keyframes:[
+//     {x:0, y:0, duration:0.5},
+//     {x:200, y:0, duration:0.5},
+//     {x:200, y:200, duration:0.5}
+// ]})
+//keyframes PERCENTs
+.to(".red-box",{
+    keyframes:{
+    "0%":{x:0, y:0},
+    "25%":{x:200, y:0},
+    "50%":{x:200, y:200},
+    "75%":{x:0, y:200},
+    "100%":{x:0, y:0}
+    },
+duration:2})
 
 ;
